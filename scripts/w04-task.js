145a75ea13d9c4c33d18523bc02a3cad31d5b118
kapolojj <johanneskapolok@gmail.com>
2023-10-07 23:39:23 +0200

    final touch ups

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -2,32 +2,29 @@
 
 /* Profile Object  */
 let myProfile = {
-    name: "Johannes Kapolo"
-    
-    // photo: 
+    name: "Johannes Kapolo",
 
-    // favoriteFoods: [
-    //     'Rice',
-    //     'Tikka Masala',
-    //     'Prioshki',
-    //     'Shrimp',
-    //     'Banana Cream Pie'
-    // ],
+    photo: 30,
 
-    // hobbies: [
-    //     'Soccer',
-    //     'Sprinting',
-    //     'Gaming',
-    //     'Hiking',
-    //     'Basketball'
-    // ],
-    // placesLived:[
+    favoriteFoods: [
+        'Rice',
+        'Tikka Masala',
+        'Prioshki',
+        'Shrimp',
+        'Banana Cream Pie'
+    ],
+    hobbies: [
+        'Soccer',
+        'Sprinting',
+        'Gaming',
+        'Hiking',
+        'Basketball'
+    ],
+    placesLived:[
         
 
-    // ],
+    ],
 };
-document.querySelector('#name').textContent = myProfile.name;
-
 
 
 
@@ -36,23 +33,30 @@ document.querySelector('#name').textContent = myProfile.name;
 /* Populate Profile Object with placesLive objects */
 myProfile.placesLived.push(
     {
-        place: 'San Francisco, CA',
+        place: '🏠 Namibia, Windhoek',
+        length:'2 year'
+    },
+    {
+        place: '🏠 Nigeria, Lagos',
         length:'1 year'
+    },
+    {
+        place: ' 🏠 Durban, South Africa',
+        length:'2 year'
     }
 );
 
 
 
 /* DOM Manipulation - Output */
-// document.querySelector('#name').textContent = myProfile.name;
-
 /* Name */
+document.querySelector('#name').textContent = myProfile.name;
 
 /* Photo with attributes */
 
 
 /* Favorite Foods List*/
-myProfile.facourateFood.forEach(food =>{
+myProfile.favoriteFoods.forEach(food =>{
     let li = document.createElement('li');
     li.textContent=food;
     document.querySelector('#favorite-foods').appendChild(li);
@@ -60,8 +64,23 @@ myProfile.facourateFood.forEach(food =>{
 
 
 /* Hobbies List */
+myProfile.hobbies.forEach(joy =>{
+    let li = document.createElement('li');
+    li.textContent=joy;
+    document.querySelector('#hobbies').appendChild(li);
+});
 
 
 /* Places Lived DataList */
 
+let placesLivedList = document.getElementById('places-lived');
+
+myProfile.placesLived.forEach(function (placeObj) {
+    let dt = document.createElement('dt');
+    let dd = document.createElement('dd');
+    dt.textContent = placeObj.place;
+    dd.textContent = placeObj.length;
+    placesLivedList.appendChild(dt);
+    placesLivedList.appendChild(dd);
+});
 
