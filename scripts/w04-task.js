4dad45c521f0485b62a15240b8165cf91274e7b8
kapolojj <johanneskapolok@gmail.com>
2023-10-07 07:03:12 +0200

    1.0

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,16 +1,50 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
+let myProfile = {
+    name: "Johannes Kapolo"
+    
+    // photo: 
+
+    // favoriteFoods: [
+    //     'Rice',
+    //     'Tikka Masala',
+    //     'Prioshki',
+    //     'Shrimp',
+    //     'Banana Cream Pie'
+    // ],
+
+    // hobbies: [
+    //     'Soccer',
+    //     'Sprinting',
+    //     'Gaming',
+    //     'Hiking',
+    //     'Basketball'
+    // ],
+    // placesLived:[
+        
+
+    // ],
+};
+document.querySelector('#name').textContent = myProfile.name;
+
+
 
 
 
 
 /* Populate Profile Object with placesLive objects */
-
+myProfile.placesLived.push(
+    {
+        place: 'San Francisco, CA',
+        length:'1 year'
+    }
+);
 
 
 
 /* DOM Manipulation - Output */
+// document.querySelector('#name').textContent = myProfile.name;
 
 /* Name */
 
@@ -18,6 +52,11 @@
 
 
 /* Favorite Foods List*/
+myProfile.facourateFood.forEach(food =>{
+    let li = document.createElement('li');
+    li.textContent=food;
+    document.querySelector('#favorite-foods').appendChild(li);
+});
 
 
 /* Hobbies List */
